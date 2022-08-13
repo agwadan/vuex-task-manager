a8b2a4a3b684c891af436bea7648b37e027577a1
agwadan <agwa.dan@gmail.com>
2022-08-13 10:40:51 +0300

    Add Todo Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <ToDos/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDos from './components/ToDos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDos
   }
 }
 </script>
